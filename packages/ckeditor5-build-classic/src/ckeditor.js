f8ee74954425fe9533232414d26d81f970a1ca9f
baghishani <reza.baghishani@gmail.com>
2021-11-30 21:38:59 +0330

    all plugin added

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -1,68 +1,210 @@
 /**
- * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
+import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder.js';
+import CKFinderUploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js';
+import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
+import Comments from '@ckeditor/ckeditor5-comments/src/comments.js';
+import DataFilter from '@ckeditor/ckeditor5-html-support/src/datafilter.js';
+import DataSchema from '@ckeditor/ckeditor5-html-support/src/dataschema.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import ExportPdf from '@ckeditor/ckeditor5-export-pdf/src/exportpdf.js';
+import ExportWord from '@ckeditor/ckeditor5-export-word/src/exportword.js';
+import FindAndReplace from '@ckeditor/ckeditor5-find-and-replace/src/findandreplace.js';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
+import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed.js';
+import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert.js';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle.js';
+import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown.js';
+import MathType from '@wiris/mathtype-ckeditor5';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
+import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak.js';
+import Pagination from '@ckeditor/ckeditor5-pagination/src/pagination.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
+import RevisionHistory from '@ckeditor/ckeditor5-revision-history/src/revisionhistory.js';
+import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
+import SpecialCharactersArrows from '@ckeditor/ckeditor5-special-characters/src/specialcharactersarrows.js';
+import SpecialCharactersCurrency from '@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency.js';
+import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials.js';
+import SpecialCharactersLatin from '@ckeditor/ckeditor5-special-characters/src/specialcharacterslatin.js';
+import SpecialCharactersMathematical from '@ckeditor/ckeditor5-special-characters/src/specialcharactersmathematical.js';
+import SpecialCharactersText from '@ckeditor/ckeditor5-special-characters/src/specialcharacterstext.js';
+import StandardEditingMode from '@ckeditor/ckeditor5-restricted-editing/src/standardeditingmode.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableCaption from '@ckeditor/ckeditor5-table/src/tablecaption.js';
+import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
+import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import TextPartLanguage from '@ckeditor/ckeditor5-language/src/textpartlanguage.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
+import TrackChanges from '@ckeditor/ckeditor5-track-changes/src/trackchanges.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
+import WProofreader from '@webspellchecker/wproofreader-ckeditor5/src/wproofreader.js';
+import EditorWatchdog from '@ckeditor/ckeditor5-watchdog/src/editorwatchdog.js';
 
-// The editor creator to use.
-import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+class RevisionHistoryAdapter {
+	static get requires() {
+		return [ 'RevisionHistory' ];
+	}
 
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
-import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+	constructor( editor ) {
+		this.editor = editor;
+	}
 
-export default class ClassicEditor extends ClassicEditorBase {}
+	init() {
+		const editor = this.editor;
+		const revisionHistory = editor.plugins.get( 'RevisionHistory' );
+		const revisionsData = [];
+
+		revisionHistory.adapter = {
+			getRevisions: async () => {
+				const revisionsNoData = revisionsData.map( revisionData => {
+					const filtered = { ...revisionData };
+
+					delete filtered.data;
+
+					return filtered;
+				} );
+
+				return revisionsNoData;
+			},
+			getRevision: async ( { revisionId } ) => {
+				return revisionsData.find( data => data.id == revisionId );
+			},
+			updateRevision: async revisionData => {
+				const revision = revisionsData.find( data => data.id == revisionData.id );
+
+				for ( const i in revisionData ) {
+					revision[ i ] = revisionData[ i ];
+				}
+			},
+			addRevision: async revisionData => {
+				revisionsData.push( revisionData );
+			}
+		};
+	}
+}
+
+class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
-ClassicEditor.builtinPlugins = [
-	Essentials,
-	UploadAdapter,
+Editor.builtinPlugins = [
+	Alignment,
 	Autoformat,
-	Bold,
-	Italic,
+	AutoImage,
+	AutoLink,
 	BlockQuote,
+	Bold,
 	CKFinder,
+	CKFinderUploadAdapter,
 	CloudServices,
-	EasyImage,
+	Code,
+	CodeBlock,
+	Comments,
+	DataFilter,
+	DataSchema,
+	Essentials,
+	ExportPdf,
+	ExportWord,
+	FindAndReplace,
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+	GeneralHtmlSupport,
 	Heading,
+	Highlight,
+	HorizontalLine,
+	HtmlEmbed,
 	Image,
 	ImageCaption,
+	ImageInsert,
+	ImageResize,
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
 	Indent,
+	IndentBlock,
+	Italic,
 	Link,
+	LinkImage,
 	List,
+	ListStyle,
+	Markdown,
+	MathType,
 	MediaEmbed,
+	MediaEmbedToolbar,
+	Mention,
+	PageBreak,
+	Pagination,
 	Paragraph,
 	PasteFromOffice,
+	RevisionHistory,
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
+	SpecialCharactersText,
+	StandardEditingMode,
+	Strikethrough,
+	Subscript,
+	Superscript,
 	Table,
+	TableCaption,
+	TableCellProperties,
+	TableProperties,
 	TableToolbar,
-	TextTransformation
+	TextPartLanguage,
+	TextTransformation,
+	TodoList,
+	TrackChanges,
+	Underline,
+	WordCount,
+	WProofreader,
+	RevisionHistoryAdapter
 ];
 
 // Editor configuration.
-ClassicEditor.defaultConfig = {
+Editor.defaultConfig = {
 	toolbar: {
 		items: [
 			'heading',
@@ -76,31 +218,80 @@ ClassicEditor.defaultConfig = {
 			'outdent',
 			'indent',
 			'|',
-			'uploadImage',
+			'imageUpload',
 			'blockQuote',
 			'insertTable',
 			'mediaEmbed',
 			'undo',
-			'redo'
+			'redo',
+			'textPartLanguage',
+			'CKFinder',
+			'alignment',
+			'code',
+			'codeBlock',
+			'exportPdf',
+			'exportWord',
+			'comment',
+			'findAndReplace',
+			'fontBackgroundColor',
+			'fontColor',
+			'fontFamily',
+			'fontSize',
+			'highlight',
+			'horizontalLine',
+			'htmlEmbed',
+			'imageInsert',
+			'MathType',
+			'ChemType',
+			'pageBreak',
+			'previousPage',
+			'nextPage',
+			'pageNavigation',
+			'revisionHistory',
+			'specialCharacters',
+			'restrictedEditingException',
+			'superscript',
+			'subscript',
+			'strikethrough',
+			'todoList',
+			'wproofreader',
+			'underline',
+			'trackChanges'
 		]
 	},
+	language: 'fa',
 	image: {
 		toolbar: [
+			'comment',
+			'imageTextAlternative',
 			'imageStyle:inline',
 			'imageStyle:block',
 			'imageStyle:side',
-			'|',
-			'toggleImageCaption',
-			'imageTextAlternative'
+			'linkImage'
 		]
 	},
 	table: {
 		contentToolbar: [
 			'tableColumn',
 			'tableRow',
-			'mergeTableCells'
+			'mergeTableCells',
+			'tableCellProperties',
+			'tableProperties'
+		],
+		tableToolbar: [
+			'comment'
 		]
 	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	pagination: {
+		pageWidth: '21cm',
+		pageHeight: '29.7cm',
+		pageMargins: {
+			top: '20mm',
+			bottom: '20mm',
+			left: '12mm',
+			right: '12mm'
+		}
+	}
 };
+
+export default { Editor, EditorWatchdog };
